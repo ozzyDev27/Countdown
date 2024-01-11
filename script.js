28ccbfa8d29e71cd489a0c19fd0a41af4907097e
ozzy <businessozzy27@gmail.com>
2024-01-11 10:08:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ function timeUntil() {
 	const now = new Date();
 	const targetTime = new Date(now);
 	targetTime.setHours(10, 47, 25, 0);
-    if (now>targetTime){
-        const mainTextElement = document.querySelector('.mainText');
-        mainTextElement.innerHTML = "Done!"; 
-    }
-    else{
-        const timeDifference = targetTime - now;
-        const minutes = Math.floor(timeDifference / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-        const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-        const mainTextElement = document.querySelector('.mainText');
-        mainTextElement.innerHTML = formattedTime;
-    }
+	if (now > targetTime) {
+		const mainTextElement = document.querySelector('.mainText');
+		mainTextElement.innerHTML = "Done!";
+	} else {
+		const timeDifference = targetTime - now;
+		const minutes = Math.floor(timeDifference / (1000 * 60));
+		const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+		const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+		const mainTextElement = document.querySelector('.mainText');
+		mainTextElement.innerHTML = formattedTime;
+	}
 }
 
 setInterval(timeUntil, 1000);
+
+document.addEventListener('DOMContentLoaded', () => {
+	const mainTextElement = document.querySelector('.mainText');
+	mainTextElement.addEventListener('click', () => {
+		if (mainTextElement.requestFullscreen) {
+			mainTextElement.requestFullscreen();
+		} else if (mainTextElement.mozRequestFullScreen) { /* Firefox */
+			mainTextElement.mozRequestFullScreen();
+		} else if (mainTextElement.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+			mainTextElement.webkitRequestFullscreen();
+		} else if (mainTextElement.msRequestFullscreen) { /* IE/Edge */
+			mainTextElement.msRequestFullscreen();
+		}
+	});
+});
