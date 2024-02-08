d2ff7fb0c8d97dcbebc9c2a109bc1f9dddb5b515
ozzy <businessozzy27@gmail.com>
2024-02-08 09:51:39 -0500

    multiple times

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
+let closestTimeIndex = 0; // Declare closestTimeIndex outside the function to keep track of the current index
+let countdownEndTime = null; // Variable to track the time when countdown ends
+
 function timeUntil() {
     const now = new Date();
-    const targetTime = new Date(now);
-    targetTime.setHours(14, 45, 0, 0);
-    if (now > targetTime) {
+    const Times = [[9, 20, 0, true], [9,22,0,false], [10, 47, 27, true], [10,49,27,false], [12, 3, 6, true], [12,5,6,false], [13, 27, 42,true],[13,29,42,false], [14, 45, 0,true]];
+
+    // Convert Times array into an array of Date objects
+    const timesInDateObjects = Times.map(time => {
+        const [hour, minute, second] = time;
+        const date = new Date();
+        date.setHours(hour, minute, second, 0);
+        return date;
+    });
+
+    // Check if current time exceeds target time
+    if (now > timesInDateObjects[closestTimeIndex]) {
+        // Find the next closest time from Times array
+        closestTimeIndex++;
+        if (closestTimeIndex >= Times.length) {
+            closestTimeIndex = 0; // Reset index if all times have passed
+        }
+    }
+
+    if (now > timesInDateObjects[closestTimeIndex] && now-timesInDateObjects[closestTimeIndex-1]<=120000) {
+        const mainTextElement = document.querySelector('.mainText');
+        mainTextElement.innerHTML = "Done!";}
+    else if (!Times[closestTimeIndex][3]) {
         const mainTextElement = document.querySelector('.mainText');
         mainTextElement.innerHTML = "Done!";
-    } else {
+    }
+    else {
+        const targetTime = new Date(timesInDateObjects[closestTimeIndex]);
         const timeDifference = targetTime - now;
         const minutes = Math.floor(timeDifference / (1000 * 60));
         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
@@ -23,10 +48,10 @@ function adjustMainText() {
     const screenWidth = window.innerWidth;
     if (screenHeight > screenWidth) {
         mainTextElement.style.transform = 'rotate(270deg)';
-        mainTextElement.style.fontSize = '20vh'; // Set font size to 10vh if rotated
+        mainTextElement.style.fontSize = '20vh';
     } else {
         mainTextElement.style.transform = 'none';
-        mainTextElement.style.fontSize = '10vw'; // Set font size to 10vw if not rotated
+        mainTextElement.style.fontSize = '15vw';
     }
 }
 
