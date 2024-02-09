b980da972d035feda0dec6acb6c7800247c0a9b7
ozzy <businessozzy27@gmail.com>
2024-02-08 20:49:53 -0500

    stuff

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let countdownEndTime = null; // Variable to track the time when countdown ends
 
 function timeUntil() {
     const now = new Date();
-    const Times = [[9, 20, 0, true], [9,22,0,false], [10, 47, 27, true], [10,49,27,false], [11, 30, 6, true], [12, 3, 6, true], [12,5,6,false], [13, 27, 42,true],[13,29,42,false], [14, 45, 0,true]];
+    const Times = [[9, 20, 0, true], [9,22,0,false], [10, 47, 27, true], [10,49,27,false], [11, 30, 6, true], [12, 3, 6, true], [12,5,6,false], [13, 27, 42,true],[13,29,42,false], [14, 45, 0,true], [24,0,0,false]];
 
     // Convert Times array into an array of Date objects
     const timesInDateObjects = Times.map(time => {
@@ -14,7 +14,7 @@ function timeUntil() {
     });
 
     // Check if current time exceeds target time
-    if (now > timesInDateObjects[closestTimeIndex]) {
+    while (now > timesInDateObjects[closestTimeIndex]) {
         // Find the next closest time from Times array
         closestTimeIndex++;
         if (closestTimeIndex >= Times.length) {
@@ -22,14 +22,13 @@ function timeUntil() {
         }
     }
 
-    if (now > timesInDateObjects[closestTimeIndex] && now-timesInDateObjects[closestTimeIndex-1]<=120000) {
-        const mainTextElement = document.querySelector('.mainText');
-        mainTextElement.innerHTML = "Done!";}
-    else if (!Times[closestTimeIndex][3]) {
+    if (now > timesInDateObjects[closestTimeIndex] && now - timesInDateObjects[closestTimeIndex - 1] <= 120000) {
         const mainTextElement = document.querySelector('.mainText');
         mainTextElement.innerHTML = "Done!";
-    }
-    else {
+    } else if (!Times[closestTimeIndex][3]) {
+        const mainTextElement = document.querySelector('.mainText');
+        mainTextElement.innerHTML = "Done!";
+    } else {
         const targetTime = new Date(timesInDateObjects[closestTimeIndex]);
         const timeDifference = targetTime - now;
         const minutes = Math.floor(timeDifference / (1000 * 60));
@@ -55,19 +54,23 @@ function adjustMainText() {
     }
 }
 
-setInterval(timeUntil, 200);
-
+// Initialize mainTextElement with an initial value
 document.addEventListener('DOMContentLoaded', () => {
     const mainTextElement = document.querySelector('.mainText');
-    mainTextElement.addEventListener('click', () => {
-        if (mainTextElement.requestFullscreen) {
-            mainTextElement.requestFullscreen();
-        } else if (mainTextElement.mozRequestFullScreen) { /* Firefox */
-            mainTextElement.mozRequestFullScreen();
-        } else if (mainTextElement.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    mainTextElement.innerHTML = "Loading..."; // Show loading message initially
+
+    // Function to handle entering fullscreen
+    function enterFullscreen() {
+        if (mainTextElement.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
             mainTextElement.webkitRequestFullscreen();
-        } else if (mainTextElement.msRequestFullscreen) { /* IE/Edge */
-            mainTextElement.msRequestFullscreen();
         }
-    });
+    }
+
+    // Start the countdown loop after a brief delay to allow rendering of initial value 
+    setTimeout(() => {
+        setInterval(timeUntil, 200);
+    }, 500);
+
+    // Fullscreen functionality with background color adjustment
+    mainTextElement.addEventListener('click', enterFullscreen);
 });
