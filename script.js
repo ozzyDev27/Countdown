833ea1771252d7b25d842f7148567252b0eda85e
ozzy <businessozzy27@gmail.com>
2024-03-22 14:18:55 -0400

    stuff

--- script.js
+++ script.js
@@ -110,8 +110,8 @@ function enterFullscreen() {
 document.addEventListener('DOMContentLoaded', () => {
     const mainTextElement = document.querySelector('.mainText');
     const now = new Date();
-    mainTextElement.style.left=Math.random()*window.innerWidth+'px';
-    mainTextElement.style.top=Math.random()*window.innerHeight+'px';
+    mainTextElement.style.left=(Math.random()*window.innerWidth-mainTextElement.offsetWidth)+'px';
+    mainTextElement.style.top=(Math.random()*window.innerHeight-mainTextElement.offsetHeight)+'px';
     if (now.getDay() === 0 || now.getDay() === 6) {
         mainTextElement.innerHTML = "Weekend :)";
     } else {
